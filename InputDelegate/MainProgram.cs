1cb35fe6754b5167493ee3da4cdb1092ff44f208
Sachin Hegde <schnhegde@gmail.com>
2023-01-05 17:30:46 +0530

    removed static from delegate definations

--- InputDelegate/MainProgram.cs
+++ InputDelegate/MainProgram.cs
@@ -7,21 +7,22 @@ namespace InputDelegate
 
         public static void Main()
         {
-            var parser = new ConsoleParser(onNumber, onWord, onJunk);
+            var program = new MainProgram();
+            var parser = new ConsoleParser(program.onNumber, program.onWord, program.onJunk);
             parser.run();
         }
         
-        private static void onWord()
+        private void onWord()
         {
             Console.WriteLine("OnWord");
         }
 
-        private static void onNumber()
+        private void onNumber()
         {
             Console.WriteLine("OnNumber");
         }
 
-        private static void onJunk()
+        private void onJunk()
         {
             Console.WriteLine("OnJunk");
         }
